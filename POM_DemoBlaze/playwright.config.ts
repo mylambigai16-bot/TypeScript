fa614057600451a89d20681a751e24fca528654b
Mylambigai <mylambigai16@gmail.com>
2026-07-01 08:29:44 +0530

    Report

--- POM_DemoBlaze/playwright.config.ts
+++ POM_DemoBlaze/playwright.config.ts
@@ -13,12 +13,13 @@ export default defineConfig({
   },
 
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   //forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['html',{open:"never"}]
+    ['html',{open:"never"}],
+    ['allure-playwright']
   ],
   use: {
     trace: 'on-first-retry',
