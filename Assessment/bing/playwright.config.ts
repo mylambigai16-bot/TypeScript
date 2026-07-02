25a666eeabdba9ee92763a9f143c2199c2de5f90
Mylambigai <mylambigai16@gmail.com>
2026-07-02 08:10:41 +0530

    report

--- Assessment/bing/playwright.config.ts
+++ Assessment/bing/playwright.config.ts
@@ -11,7 +11,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   use: {
     headless:false,
     trace: 'on-first-retry',
